6884689dc98ec52fb40ea051cc068fe9c95bc470
Your Name <143778356+gharat-purva@users.noreply.github.com>
2024-07-31 01:08:03 +0530

    easy navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import CourseApp from './Course/CourseApp';
 import NoCourseApp from './noCourse/NoCourseApp';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Select a version:</h1>
+      <button onClick={() => navigate('/course')} className="m-2 p-2 bg-cool-blue text-white rounded">Course</button>
+      <button onClick={() => navigate('/nocourse')} className="m-2 p-2 bg-purple text-white rounded">No Course</button>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -9,7 +21,7 @@ function App() {
       <Routes>
         <Route path="/course" element={<CourseApp />} />
         <Route path="/nocourse" element={<NoCourseApp />} />
-        <Route path="/" element={<div>Select a version: /course or /nocourse</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
